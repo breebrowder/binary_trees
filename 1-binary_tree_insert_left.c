d7240cf976379c4973aa215cfca095e7b57d7c3a
breezybrow <browder.bree@gmail.com>
2021-10-07 00:52:34 +0000

    Memory leak issue potentially resolved

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,10 +16,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *created_child; /* pointer to left child */
 
-	created_child = binary_tree_node(parent, value); /* initialize child */
-
 	if (parent == NULL)
 		return (NULL); /* condition: failure */
+
+	created_child = binary_tree_node(parent, value); /* initialize child */
 
 	if (created_child == NULL)
 		return (NULL); /* condition: failure */
