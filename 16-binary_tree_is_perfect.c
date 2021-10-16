f03ed2f59eae57db09187b76d1325005c1a24a9e
breezybrow <browder.bree@gmail.com>
2021-10-16 21:00:50 +0000

    Aded a little trick

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#define BTIP binary_tree_is_perfect
 
 /**
  * binary_tree_leaves- counts the leaves in a binary tree
@@ -10,21 +11,21 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-        size_t total = 0;
+	size_t total = 0;
 
-        if (tree == NULL)
-                return (0); /* condition */
+	if (tree == NULL)
+		return (0); /* condition */
 
-        if (tree->left != NULL)
-                total += binary_tree_leaves(tree->left);
+	if (tree->left != NULL)
+		total += binary_tree_leaves(tree->left);
 
-        if (tree->right != NULL)
-                total += binary_tree_leaves(tree->right);
+	if (tree->right != NULL)
+		total += binary_tree_leaves(tree->right);
 
-        if (tree->left == NULL && tree->right == NULL)
-                return (total + 1);
-        else
-                return (total);
+	if (tree->left == NULL && tree->right == NULL)
+		return (total + 1);
+	else
+		return (total);
 }
 
 /**
@@ -38,19 +39,19 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
-                return (0);
+		return (0);
 
-        /* Leaf? yes or no */
-        if (!tree->left && !tree->right)
-                return (1);
+/* Leaf? yes or no */
+	if (!tree->left && !tree->right)
+		return (1);
 
-        /* Is the # of leaves odd? */
+/* Is the # of leaves odd? */
 	if (binary_tree_leaves(tree) % 2 == 1)
 		return (0);
 
-        /* check if left and right subtrees are full */
-        if (tree->left && tree->right)
-                return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+/* check if left and right subtrees are full */
+	if (tree->left && tree->right)
+		return (BTIP(tree->left) && BTIP(tree->right));
 
 	return (0);
 }
